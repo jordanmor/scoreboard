97a061e17d37d8785639d084ef13b595967a1cdf
Jordan Mor <jordanmor@gmail.com>
2018-11-20 15:04:17 -0500

    Add the App and Player components

--- app.js
+++ app.js
@@ -7,7 +7,38 @@ const Header = () => {
   );
 }
 
+const Counter = () => {
+  return (
+    <div className='counter'>
+      <button className='counter-action decrement'> - </button>
+      <span className="counter-score">35</span>
+      <button className='counter-action increment'> + </button>
+    </div>
+  );
+}
+
+const Player = () => {
+  return (
+    <div className='player'>
+      <span className="player-name">
+        Jordan
+      </span>
+      <Counter />
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+      {/* Players list */}
+      <Player />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <Header />,
+  <App />,
   document.getElementById('root')
 );
