1940ccaac7b50274d208fa36aa15ba8cd8e23aac
Jordan Mor <jordanmor@gmail.com>
2018-12-17 14:15:03 -0500

    Add stopwatch component

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Stats from './stats';
+import StopWatch from './stopWatch';
 
 const Header = ({ players, title }) => {
   return (
     <header>
       <Stats players={players} />
       <h1>{title}</h1>
+      <StopWatch />
     </header>
   );
 }
