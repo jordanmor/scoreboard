10c7f0dc4ba1ac2b236a361e6cc4c4a2bd1d8abe
Jordan Mor <jordanmor@gmail.com>
2018-12-24 07:41:30 -0500

    Validate Counter component's props with PropTypes library

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Counter = props => {
 
@@ -12,5 +13,11 @@ const Counter = props => {
       </div>
     );
 }
+
+Counter.propTypes = {
+  index: PropTypes.number,
+  score: PropTypes.number,
+  changeScore: PropTypes.func
+};
 
 export default Counter;
